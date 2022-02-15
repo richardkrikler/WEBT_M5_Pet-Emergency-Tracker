97f8a8a205773a9b4fbe4cda70d4f8833196f63f
Hadi Tlais <8152@htl.rennweg.at>
2022-02-15 11:39:10 +0100

    Core- erster QR-Code

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require 'vendor\autoload.php';
+
+use Htlw3r\Pettracker\Model\Owner;
+
+$myOwner = new Owner('TLA','06767690278');
+
+$data = 'Name: ' .$myOwner->getName();
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data($data)
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+ //   ->logoPath('/assets/symfony.png')
+    ->labelText('This is the label')
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(new LabelAlignmentCenter())
+    ->build();
+
+// Directly output the QR code
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
+
+//// Save it to a file
+//$result->saveToFile(__DIR__.'/qrcode.png');
+//
+//// Generate a data URI to include image data inline (i.e. inside an <img> tag)
+//$dataUri = $result->getDataUri();
+
---
+++ src/Model/Owner.php
@@ -0,0 +1,38 @@
+<?php
+namespace Htlw3r\Pettracker\Model;
+
+class Owner{
+
+private string $name;
+private string $phonenumber;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhonenumber(): string
+    {
+        return $this->phonenumber;
+    }
+
+    /**
+     * @param string $name
+     * @param string $phonenumber
+     */
+    public function __construct(string $name, string $phonenumber)
+    {
+        $this->name = $name;
+        $this->phonenumber = $phonenumber;
+    }
+
+
+}
+
+
