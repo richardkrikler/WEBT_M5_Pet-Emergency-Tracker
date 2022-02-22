a222ea6ac12cf50591ccc354e6675876a6a4d7bd
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-02-22 09:17:51 +0100

    Added regex check for phone number

--- index.php
+++ index.php
@@ -12,28 +12,33 @@ use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\PngWriter;
 
+$phoneCheck = 1;
 
 if (isset($_GET['name']) && isset($_GET['phonenumber'])) {
-    $myOwner = new Owner($_GET['name'], $_GET['phonenumber']);
+    $phoneCheck = preg_match('/^[+]\d{1,3}\s?\d{3}\s?\d{3}\s?\d{4}$/', $_GET['phonenumber']);
 
-    $data = "Hello! My name is HONEYPIE, I'm the Cat of {$myOwner->getName()}, please call {$myOwner->getPhonenumber()} - he'll be so glad!";
+    if ($phoneCheck === 1) {
+        $myOwner = new Owner($_GET['name'], $_GET['phonenumber']);
 
-    $result = Builder::create()
-        ->writer(new PngWriter())
-        ->writerOptions([])
-        ->data($data)
-        ->encoding(new Encoding('UTF-8'))
-        ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
-        ->size(300)
-        ->margin(10)
-        ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
-        ->labelText('Pet-Emergency-Tracker')
-        ->labelFont(new NotoSans(20))
-        ->labelAlignment(new LabelAlignmentCenter())
-        ->build();
+        $data = "Hello! My name is HONEYPIE, I'm the Cat of {$myOwner->getName()}, please call {$myOwner->getPhonenumber()} - he'll be so glad!";
 
-    header('Content-Type: ' . $result->getMimeType());
-    echo $result->getString();
+        $result = Builder::create()
+            ->writer(new PngWriter())
+            ->writerOptions([])
+            ->data($data)
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->size(300)
+            ->margin(10)
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->labelText('Pet-Emergency-Tracker')
+            ->labelFont(new NotoSans(20))
+            ->labelAlignment(new LabelAlignmentCenter())
+            ->build();
+
+        header('Content-Type: ' . $result->getMimeType());
+        echo $result->getString();
+    }
 }
 ?>
 
@@ -54,10 +59,32 @@ if (isset($_GET['name']) && isset($_GET['phonenumber'])) {
             Pet Emergency Tracker
         </h1>
     </div>
-    <form method="get" class="bg-amber-600 rounded-md p-10 drop-shadow-md text-4xl my-10 mx-10 flex flex-col sm:w-7/12 w-11/12">
-        <input type="text" name="name" class="bg-amber-100 p-5 rounded-md drop-shadow-md placeholder-amber-400 text-amber-600" placeholder="Name" required>
-        <input type="text" name="phonenumber" class="bg-amber-100 my-10 p-5 rounded-md drop-shadow-md placeholder-amber-400 text-amber-600" placeholder="Phone-number" required>
-        <input type="submit" value="Submit" class="bg-amber-400 p-5 font-bold text-amber-700 rounded-md drop-shadow-md hover:drop-shadow-xl ease-in duration-200">
+
+    <?php
+    if ($phoneCheck === 0) {
+        echo <<<PHONE_ERROR
+    <div role="alert" class="bg-amber-600 rounded-md drop-shadow-md m-5 sm:w-5/12 w-11/12">
+        <div class="bg-red-500 text-white font-bold rounded-t-md px-4 py-2">
+            Error
+        </div>
+        <div class="border border-t-0 border-red-400 rounded-b-md bg-red-100 px-4 py-3 text-red-700">
+            <p>Incorrect Phone Number!</p>
+        </div>
+    </div>
+PHONE_ERROR;
+    }
+    ?>
+
+    <form method="get"
+          class="bg-amber-600 rounded-md p-10 drop-shadow-md text-4xl my-10 mx-10 flex flex-col sm:w-7/12 w-11/12">
+        <input type="text" name="name"
+               class="bg-amber-100 p-5 rounded-md drop-shadow-md placeholder-amber-400 text-amber-600"
+               placeholder="Name" required>
+        <input type="text" name="phonenumber"
+               class="bg-amber-100 my-10 p-5 rounded-md drop-shadow-md placeholder-amber-400 text-amber-600"
+               placeholder="Phone-number" required>
+        <input type="submit" value="Submit"
+               class="bg-amber-400 p-5 font-bold text-amber-700 rounded-md drop-shadow-md hover:drop-shadow-xl ease-in duration-200">
     </form>
 </div>
 
