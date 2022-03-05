544fbadfdf0fd6c7d4ba2e9af293f1629ad6408b
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-03-05 11:02:02 +0100

    Translator test

---
+++ demo_translation.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+
+
+$translator = new Translator('fr_FR');
+$translator->addLoader('array', new ArrayLoader());
+$translator->addResource('array', [
+    'Hello World!' => 'Bonjour !',
+], 'fr_FR');
+
+echo $translator->trans('Hello World!'); // outputs « Bonjour ! »
+
